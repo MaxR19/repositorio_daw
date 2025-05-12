b86f10ce1ac688f57def657e9b53258911985689
MaxR19 <gomezsuarez.tonnyf2020@gmail.com>
2025-05-12 12:21:25 +0200

    Commit JS 12.05.2025

--- 3era_Eva/EntornosDesarrollo/UML/04_animales/animales.js
+++ 3era_Eva/EntornosDesarrollo/UML/04_animales/animales.js
@@ -1,3 +1,41 @@
 /*
     3 clases: Animal(nombre, hablar()), Gato(), Perro()
 */
+
+class Animales {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+
+    hablar(){
+        console.log(`${this.nombre} gruñe`);
+    }
+}
+
+class Gato extends Animales {
+    constructor(nombre) {
+        super(nombre);
+    }
+
+    hablar(){
+        console.log(`${this.nombre} maúlla.`);
+    }
+}
+
+class Perro extends Animales {
+    constructor(nombre) {
+        super(nombre);
+    }
+
+    hablar(){
+        console.log(`${this.nombre} ladra.`);
+    }
+}
+
+const animal = new Animales("El perro");
+const gato = new Gato("Meowth");
+const perro = new Perro("Max");
+
+animal.hablar();
+gato.hablar();
+perro.hablar();
