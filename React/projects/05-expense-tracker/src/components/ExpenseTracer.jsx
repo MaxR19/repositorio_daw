cce72df79fb16a8891f1c6f81c375730835b373e
MaxR19 <gomezsuarez.tonnyf2020@gmail.com>
2025-06-16 13:59:46 +0200

    React - Expense Tracker - Estilos formulario

--- React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
+++ React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
@@ -153,7 +153,7 @@ function ExpenseTracker () {
                                                     <p className='et-expense-datetime'>{expense.date} a las {expense.time}</p>
                                                 </div>
                                             </div>
-                                            <div className='expense-amount'>
+                                            <div className='et-expense-amount'>
                                                 €{expense.amount.toFixed(2)}
                                             </div> 
                                         </div>
