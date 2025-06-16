aab8e1249b3b5772727479cc056bc74ae1c85445
MaxR19 <gomezsuarez.tonnyf2020@gmail.com>
2025-06-16 12:23:29 +0200

    React - Expense Tracker

--- React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
+++ React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
@@ -1,8 +1,21 @@
 function ExpenseTracker () {
     return (
-        <div>
-            <h1>Control de gastos</h1>
-            <p>App para controlar el gasto mensual</p>
+        <div className="expense-tracker">
+            <header className="header">
+                <h1>Control de gastos</h1>
+            </header>
+            
+            <main className="et-main-content">
+                <section className="et-form-section">
+                    <h2>Añadir gasto</h2>
+                    {/* Aquí irá nuestro formulario*/}
+                </section>
+
+                <section className="et-list-section">
+                    <h2>Lista Gastos</h2>
+                    {/* Aquí irá nuestra lista*/}
+                </section>
+            </main>
         </div>
     )
 }
