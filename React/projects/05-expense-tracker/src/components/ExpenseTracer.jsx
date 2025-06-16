51c0a7e80dda2fcf270ff394a025d75186a811f3
MaxR19 <gomezsuarez.tonnyf2020@gmail.com>
2025-06-16 13:06:21 +0200

    React - Expense Tracker- Creación del formulario

--- React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
+++ React/projects/05-expense-tracker/src/components/ExpenseTracer.jsx
@@ -10,6 +10,30 @@ function ExpenseTracker () {
     // Estado para la lista de gastos
     const [expenses, setExpenses] = useState([]);
 
+    const handleChangeDescription = (event) => {
+        const valor = event?.target?.value || '';
+
+        if(valor === '') return
+
+        setDescription(valor);
+    }
+
+    const handleChangeAmount = (event) => {
+        const valor = event?.target?.value || '';
+
+        if(valor === '') return
+
+        setDescription(valor);
+    }
+
+    const handleChangeCategory = (event) => {
+        const valor = event?.target?.value || '';
+
+        if(valor === '') return
+
+        setDescription(valor);
+    }
+
     return (
         <div className="expense-tracker">
             <header className="header">
@@ -19,9 +43,49 @@ function ExpenseTracker () {
             <main className="et-main-content">
                 <section className="et-form-section">
                     <h2>Añadir gasto</h2>
-                    <p>Descripcion: {description}</p>
-                    <p>Cantidad: {amount}</p>
-                    <p>Categoría: {category}</p>
+
+                    <form className='et-expense-form'>
+                        <div className='et-form-group'>
+                            <label htmlFor='description'> Descripción:</label>
+                            <input
+                                id='description'
+                                className='et-form-input'
+                                type='text'
+                                placeholder='¿En qué has gastado?'
+                                value={description}
+                                onChange={(event) => handleChangeDescription(event)}
+                            />
+                        </div>
+                        <div className='et-form-group'>
+                            <label htmlFor='amount'> Cantidad (€):</label>
+                            <input
+                                id='amount'
+                                className='et-form-input'
+                                type='number'
+                                step='0.01'
+                                placeholder='0.00'
+                                value={amount}
+                                onChange={(event) => handleChangeAmount(event)}
+                            />
+                        </div>
+                        <div className='et-form-group'>
+                            <label htmlFor='category'> Categoría:</label>
+                            <select
+                                id='category'
+                                className='et-form-select'
+                                value={category}
+                                onChange={(event) => handleChangeCategory(event)}
+                            >
+                                <option value='comida'>Comida</option>
+                                <option value='trassporte'>Transporte</option>
+                                <option value='entretenimiento'>Entretenimiento</option>
+                                <option value='salude'>Salud</option>
+                                <option value='otros'>Otros</option>
+                            </select>
+                        </div>
+
+                        <button tyoe='submit' className='et-submit-'>Añadir Gasto</button>
+                    </form>
                 </section>
 
                 <section className="et-list-section">
